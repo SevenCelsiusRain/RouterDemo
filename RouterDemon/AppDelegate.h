230dd11a8e0252531e3c3466de56a76a18bf70a6
wanc <chao>
2017-03-06 11:40:28 +0800

    Initial Commit

---
+++ RouterDemon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RouterDemon
+//
+//  Created by wanc on 2017/3/6.
+//  Copyright © 2017年 zdsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
